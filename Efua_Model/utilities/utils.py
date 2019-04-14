f52928bca1f369fda7febfa3fb7fae030b731108
sai-pher <arimanwd@gmail.com>
2019-04-14 16:30:45 +0000

    Issue: First app dev project broken. to be updated by name then replace with second, working project.
    Update: Added gitignore for updated app project.
    
    Status: Unstable.

--- Efua_Model/utilities/utils.py
+++ Efua_Model/utilities/utils.py
@@ -2,5 +2,5 @@ from dateutil.relativedelta import relativedelta
 
 
 def diff(t_a, t_b):
-    t_diff = relativedelta(t_b, t_a)  # later/end time comes first!
+    t_diff = relativedelta(t_b, t_a)
     return '{h}h {m}m {s}s {ms}ms'.format(h=t_diff.hours, m=t_diff.minutes, s=t_diff.seconds, ms=t_diff.microseconds)
